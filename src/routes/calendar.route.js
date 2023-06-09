f0d0d10447a6c433c5252a5db90368a7b6fa0e0a
mikhaelc09 <mikhael.chris09@gmail.com>
2023-06-10 00:06:55 +0700

    placeholder calendar route

--- src/routes/calendar.route.js
+++ src/routes/calendar.route.js
@@ -3,5 +3,9 @@ const router = express.Router()
 const calendarController = require('../controllers/calendar.controller')
 
 router.get('/', calendarController.getCalendar)
+router.post('/', calendarController.createCalendar)
+router.put('/:id', calendarController.editCalendar)
+router.delete('/:id', calendarController.deleteCalendar)
+router.get('/next', calendarController.getNextEvent)
 
 module.exports = router
