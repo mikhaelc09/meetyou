ec9d6dcd69c289cc11d0f771a52d72d9f002f8b0
mikhaelc09 <mikhael.chris09@gmail.com>
2023-06-10 00:06:30 +0700

    placeholder calendar endpoint

--- src/controllers/calendar.controller.js
+++ src/controllers/calendar.controller.js
@@ -1,8 +1,27 @@
 module.exports = {
     getCalendar: (req, res) => {
         return res.status(200).json({
-            message: 'GET calendar'
+            message: 'OK'
         })
-    }
-
+    },
+    createCalendar: (req, res) => {
+        return res.status(200).json({
+            message: 'OK'
+        })
+    },
+    editCalendar: (req, res) => {
+        return res.status(200).json({
+            message: 'OK'
+        })
+    },
+    deleteCalendar: (req, res) => {
+        return res.status(200).json({
+            message: 'OK'
+        })
+    },
+    getNextEvent: (req, res) => {
+        return res.status(200).json({
+            message: 'OK'
+        })
+    },
 }
