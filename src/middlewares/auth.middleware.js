eb929b958efce96a653c207aa6ce6712705334e4
mikhaelc09 <mikhael.chris09@gmail.com>
2023-06-22 22:47:00 +0700

    create check google key middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const env = require('../config/env.config.js');
 const db = require('../models/index.js');
+const oauth2Client = require('../config/google_oauth.config.js');
 
 module.exports = {
     authToken: (req, res, next) => {
@@ -27,6 +28,29 @@ module.exports = {
         }
         return res.status(401).json({ error: "Invalid Zoom Key, Please Update Your Zoom Key Using [PUT] /account/zoom or follow this tutorial: " });
     },
+
+    checkGoogleKey: async (req, res, next) => {
+        const user = await db.User.findOne({
+            where: { email: req.user.email }
+        });
+        if (!user.google_code) {
+            return res.status(401).json({ error: "No Google OAuth 2.0 Key, Please Login from [POST] /v1/auth/auth_google or follow this tutorial: " });
+        }
+        if(!oauth2Client.credentials.access_token){
+            let tokens
+            try{
+                tokens = await oauth2Client.getToken(user.google_code)
+            }
+            catch(err){
+                console.log(err)
+                return res.status(400).json({ error: "No Google OAuth 2.0 Key, Please Login from [POST] /v1/auth/auth_google or follow this tutorial: " })
+            }
+            console.log(tokens)
+            oauth2Client.setCredentials(tokens)
+            return next();
+        }
+        return next();
+    }
 }
 
 
