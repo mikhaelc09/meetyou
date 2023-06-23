e96507fe79f23369f74bee219eaccb32810d0287
mikhaelc09 <mikhael.chris09@gmail.com>
2023-06-23 12:26:17 +0700

    add middleware on calendar routes

--- src/routes/calendar.route.js
+++ src/routes/calendar.route.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const calendarController = require('../controllers/calendar.controller')
+const calendarController = require('../controllers/calendar.controller.js')
+const auth = require('../middlewares/auth.middleware.js')
 
-router.get('/', calendarController.getCalendar)
-router.post('/', calendarController.createCalendar)
-router.put('/:id', calendarController.editCalendar)
-router.delete('/:id', calendarController.deleteCalendar)
-router.get('/next', calendarController.getNextEvent)
+router.get('/', [auth.authToken, auth.checkGoogleKey],  calendarController.getCalendar)
+router.post('/', [auth.authToken, auth.checkGoogleKey], calendarController.createCalendar)
+router.put('/:id', [auth.authToken, auth.checkGoogleKey], calendarController.editCalendar)
+router.delete('/:id', [auth.authToken, auth.checkGoogleKey], calendarController.deleteCalendar)
 
 module.exports = router
